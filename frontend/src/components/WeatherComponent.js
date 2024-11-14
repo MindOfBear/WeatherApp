8b1ba0affae62d3d43fb8414f8e207abf9692cf5
Ovidiu <ovidiunoc@gmail.com>
2024-11-15 01:18:03 +0200

    Interface for Desktop & Mobile

--- frontend/src/components/WeatherComponent.js
+++ frontend/src/components/WeatherComponent.js
@@ -21,26 +21,46 @@ function WeatherComponent() {
   };
 
   return (
-    <div>
-      <h1>Weather</h1>
-      <input
-        type="text"
-        value={city}
-        onChange={handleInputChange}
-        placeholder="Enter city name"
-      />
-      <button onClick={getWeather}>Search</button>
-
-      {weatherData && (
-        <div>
-          <h2>Weather Data for {city}</h2>
-          <p>Temperature: {weatherData.main?.temp}°C</p>
-          <p>Weather: {weatherData.weather?.[0]?.description}</p>
-          <p>Humidity: {weatherData.main?.humidity}%</p>
-          <p>Wind Speed: {weatherData.wind?.speed} m/s</p>
+    <div className='h-screen flex justify-center items-center bg-orange-50'>
+      <div className='bg-slate-200 sm:w-1/4 rounded-3xl p-1 shadow-sm border-2 border-slate-400'>
+        <h1 className='text-center p-3 font-sans text-4xl'>Weather</h1>
+        <div className='justify-center items-center'>
+        <input 
+          className='sm:w-3/4 p-2 m-2 rounded-3xl border-gray-100 border-2'
+          type="text"
+          value={city}
+          onChange={handleInputChange}
+          placeholder="Enter city name"
+        />
+        <button className='bg-blue-200 border border-slate-400 rounded-2xl p-2' onClick={getWeather}>Search</button>
         </div>
-      )}
+        {weatherData && (
+        <div className='grid grid-cols-2 gap-2 items-center text-center font-sans text-lg'>
+          
+          <div className='bg-blue-50 w-46 h-32 rounded-2xl border-2 border-blue-200 text-center flex items-center justify-center text-4xl'>
+            <p>{weatherData.main?.temp}°C</p>
+          </div>
+
+          <div className='bg-blue-50 w-46 h-32 rounded-2xl border-2 border-blue-200 text-center flex items-center justify-center text-xl'>
+            <p>Conditions <br/> {weatherData.weather?.[0]?.description}</p>
+          </div>
+
+          <div className='bg-blue-50 w-46 h-32 rounded-2xl border-2 border-blue-200 text-center flex items-center justify-center text-xl'>
+            <p>Humidity <br/> {weatherData.main?.humidity}%</p>
+          </div>
+
+          <div className='bg-blue-50 w-46 h-32 rounded-2xl border-2 border-blue-200 text-center flex items-center justify-center text-xl'>
+            <p>Wind Speed <br/> {weatherData.wind?.speed} m/s</p>
+          </div>
+
+        </div>
+        )}
+
+        {/* toDo: load metadata dynamic */}
+
+      </div>
     </div>
+      
   );
 }
 
