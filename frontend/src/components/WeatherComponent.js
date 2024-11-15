18a8e2242214d0e738e4a84b3aac2c0fecaf9ea1
Ovidiu <ovidiunoc@gmail.com>
2024-11-15 14:12:04 +0200

    Fixed some design issues

--- frontend/src/components/WeatherComponent.js
+++ frontend/src/components/WeatherComponent.js
@@ -22,40 +22,56 @@ function WeatherComponent() {
 
   return (
     <div className='h-screen flex justify-center items-center bg-orange-50'>
-      <div className='bg-slate-200 sm:w-1/4 rounded-3xl p-1 shadow-sm border-2 border-slate-400'>
+      <div className='bg-slate-200  rounded-3xl p-1 shadow-sm border-2 border-slate-400'>
         <h1 className='text-center p-3 font-sans text-4xl'>Weather</h1>
-        <div className='justify-center items-center'>
-        <input 
-          className='sm:w-3/4 p-2 m-2 rounded-3xl border-gray-100 border-2'
-          type="text"
-          value={city}
-          onChange={handleInputChange}
-          placeholder="Enter city name"
-        />
-        <button className='bg-blue-200 border border-slate-400 rounded-2xl p-2' onClick={getWeather}>Search</button>
+        <div className="grid grid-cols-2 items-center gap-4 p-4">
+          <input 
+            className="p-2 sm:w-[140%] w-[120%] rounded-3xl border border-gray-300 justify-self-start"
+            type="text"
+            value={city}
+            onChange={handleInputChange}
+            placeholder="Enter city name"
+          />
+          <div className="flex justify-end gap-2">
+            <button 
+              className="bg-blue-200 border border-slate-400 rounded-2xl p-2"
+              onClick={getWeather}
+            >
+              Search
+            </button>
+            <button 
+              className="bg-blue-200 border border-slate-400 rounded-2xl p-2 w-10"
+              onClick={getWeather}
+            >
+              🌍
+            </button>
+          </div>
         </div>
-        {weatherData && (
-        <div className='grid grid-cols-2 gap-2 items-center text-center font-sans text-lg'>
-          
-          <div className='bg-blue-50 w-46 h-32 rounded-2xl border-2 border-blue-200 text-center flex items-center justify-center text-4xl'>
-            <p>{weatherData.main?.temp}°C</p>
-          </div>
-
-          <div className='bg-blue-50 w-46 h-32 rounded-2xl border-2 border-blue-200 text-center flex items-center justify-center text-xl'>
-            <p>Conditions <br/> {weatherData.weather?.[0]?.description}</p>
-          </div>
-
-          <div className='bg-blue-50 w-46 h-32 rounded-2xl border-2 border-blue-200 text-center flex items-center justify-center text-xl'>
-            <p>Humidity <br/> {weatherData.main?.humidity}%</p>
-          </div>
-
-          <div className='bg-blue-50 w-46 h-32 rounded-2xl border-2 border-blue-200 text-center flex items-center justify-center text-xl'>
-            <p>Wind Speed <br/> {weatherData.wind?.speed} m/s</p>
-          </div>
-
-        </div>
+        {weatherData ? (
+          weatherData && (
+            <div className='grid grid-cols-2 gap-2 items-center text-center font-sans text-lg'>
+              
+              <div className='bg-blue-50 w-46 h-32 rounded-2xl border-2 border-blue-200 text-center flex items-center justify-center text-4xl'>
+                <p>{weatherData.main?.temp}°C</p>
+              </div>
+    
+              <div className='bg-blue-50 w-46 h-32 rounded-2xl border-2 border-blue-200 text-center flex items-center justify-center text-xl'>
+                <p>Conditions <br/> {weatherData.weather?.[0]?.description}</p>
+              </div>
+    
+              <div className='bg-blue-50 w-46 h-32 rounded-2xl border-2 border-blue-200 text-center flex items-center justify-center text-xl'>
+                <p>Humidity <br/> {weatherData.main?.humidity}%</p>
+              </div>
+    
+              <div className='bg-blue-50 w-46 h-32 rounded-2xl border-2 border-blue-200 text-center flex items-center justify-center text-xl'>
+                <p>Wind Speed <br/> {weatherData.wind?.speed} m/s</p>
+              </div>
+    
+            </div>
+            )
+        ) : (
+          <div className='text-center text-slate-500 p-2 text-lg'>Please select a location!</div>
         )}
-
         {/* toDo: load metadata dynamic */}
 
       </div>
